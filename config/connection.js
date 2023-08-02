d29a13efebc598cab2f2695a46a6c74e9a755e61
romanpoudel <romanpoudel325@gmail.com>
2023-08-02 13:00:05 +0545

    dotenv file added

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 import mysql from "mysql2";
+import "dotenv/config";
 
 export default mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Sainik@28",
-  database: "testdb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
 });
