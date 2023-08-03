07df5a0838a92c6f2c7b1ae20548e65ff576263a
romanpoudel <romanpoudel325@gmail.com>
2023-08-03 10:24:10 +0545

    sequalize

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import userRouter from "./routes/userRoute.js";
+import connection from "./models/index.js";
 
 const app = express();
 app.use(express.json());
-app.use("/user",userRouter)
+app.use("/user", userRouter);
 
-app.listen(8000, () => {
+app.listen(8000, async () => {
   console.log("server has started");
+  try{
+    await connection.authenticate();
+    connection.sync();
+    // connection.sync({force:true})//this searches for table and if not found creates new using model
+    console.log("Connection has been established successfully.");
+  } catch(err){
+    console.error("Unable to connect to the database.");
+  }
 });
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,83 +1,141 @@
 import express from "express";
-import connection from "../config/connection.js";
+// import connection from "../config/connection.js";
+import connection from "../models/index.js";
+import userModel from "../models/userModel.js";
+import { Op } from "sequelize";
 
 const router = express.Router();
 
 //inserts user to the database (CREATE)
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
   const { username, location } = req.body;
-  connection.query(
-    `INSERT INTO users(username,location) VALUES(?,?)`,
-    [username, location],
-    (err, results, fields) => {
-      if (err) throw err;
-      console.log(results);
+  // connection.query(
+  //   `INSERT INTO users(username,location) VALUES(?,?)`,
+  //   [username, location],
+  //   (err, results, fields) => {
+  //     if (err) throw err;
+  //     console.log(results);
 
-      if (results.affectedRows === 1)
-        res.status(200).json({ success: true, message: "user added to DB" });
-      else
-        res
-          .status(200)
-          .json({ success: false, message: "unable to insert user" });
-    }
-  );
+  //     if (results.affectedRows === 1)
+  //       res.status(200).json({ success: true, message: "user added to DB" });
+  //     else
+  //       res
+  //         .status(200)
+  //         .json({ success: false, message: "unable to insert user" });
+  //   }
+  // );
+  try {
+    const data = await userModel.create({
+      username: username,
+      location: location,
+    });
+    console.log(data);
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 //read operation (READ)
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   console.log(id);
   if (id) {
-    connection.query(
-      `SELECT * FROM users WHERE id=${id}`,
-      (err, results, fields) => {
-        console.log(results);
-        res.status(200).json(...results);
-      }
-    );
+    // connection.query(
+    //   `SELECT * FROM users WHERE id=${id}`,
+    //   (err, results, fields) => {
+    //     console.log(results);
+    //     res.status(200).json(...results);
+    //   }
+    // );
+    const data = await userModel.findByPk(id);
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.json([]);
+    }
   } else
     res.status(200).json({ success: false, message: "user id not provided" });
 });
 
 //updates the users
-router.put("/update/:id", (req, res) => {
+router.put("/update/:id", async (req, res) => {
   const { id } = req.params;
   if (id) {
     const { username, location } = req.body;
-    connection.query(
-      `UPDATE users SET username=?, location=? WHERE id=?`,
-      [username, location, id],
-      (err, results, fields) => {
-        console.log(results);
-        if (results.affectedRows === 1) {
-          res.status(200).json({ success: true, message: "user updated" });
-        } else
-          res
-            .status(200)
-            .json({ success: false, message: "unable to update user" });
+    const data = await userModel.update(
+      { username: username, location: location },
+      {
+        where: {
+          id: id,
+        },
       }
     );
+    console.log(data);
+    if (data[0]) {
+      res.status(200).json({ success: true, message: "user updated" });
+    } else {
+      res
+        .status(200)
+        .json({ success: false, message: "unable to update user" });
+    }
+    // connection.query(
+    //   `UPDATE users SET username=?, location=? WHERE id=?`,
+    //   [username, location, id],
+    //   (err, results, fields) => {
+    //     console.log(results);
+    //     if (results.affectedRows === 1) {
+    //       res.status(200).json({ success: true, message: "user updated" });
+    //     } else
+    //       res
+    //         .status(200)
+    //         .json({ success: false, message: "unable to update user" });
+    //   }
+    // );
   } else
     res.status(200).json({ succcess: false, message: "user id not provided" });
 });
 
 //delete
-router.delete("/delete/:id", (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
   const { id } = req.params;
   if (id) {
-    connection.query(
-      `DELETE FROM users WHERE id=${id}`,
-      (err, results, fields) => {
-        console.log(results);
-        if (results.affectedRows === 1) {
-          res.status(200).json({ success: true, message: "user deleted" });
-        } else
-          res.status(200).json({ success: false, message: "id doesnot exist" });
-      }
-    );
+    const data = await userModel.destroy({
+      where: { id },
+    });
+    console.log(data);
+    if (data) {
+      res.json({ success: true, message: "user deleted successfully." });
+    } else {
+      res.json({ succcess: false, message: "couldn't delete user" });
+    }
+    // connection.query(
+    //   `DELETE FROM users WHERE id=${id}`,
+    //   (err, results, fields) => {
+    //     console.log(results);
+    //     if (results.affectedRows === 1) {
+    //       res.status(200).json({ success: true, message: "user deleted" });
+    //     } else
+    //       res.status(200).json({ success: false, message: "id doesnot exist" });
+    //   }
+    // );
   } else {
     res.status(403).json({ success: false, message: "user id not provided" });
   }
 });
+
+
+router.get("/search/by",async(req,res)=>{
+  const {location}=req.query;
+  const data=await userModel.findAll({
+    where:{
+      location:{
+        [Op.like]:`%${location}%`
+      }
+    }
+  })
+  console.log(data);
+  res.json(data);
+})
 
 export default router;
